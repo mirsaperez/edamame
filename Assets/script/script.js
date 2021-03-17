6036bd2afac0c1f8c27ed862c3aaa99e814fbc55
angeeg <angeeg@users.noreply.github.com>
2021-03-16 21:24:23 -0700

    did some psuedocode

---
+++ Assets/script/script.js
@@ -0,0 +1,12 @@
+// fetch(`https://api.edamam.com/search?q=chicken&app_id=65eb38bf&app_key=1c2548d58be7dcb81429580c548dd86d& from=0 & to=3 & calories=591-722 & health=alcohol-free`)
+//     .then(response => response.json())
+//     .then(data => console.log(data));
+
+// get reference to text field 
+var $txtField = document.querySelector("#searchBox")
+// get reference to button 
+// add an onclick evet to button 
+// that finds the value of the text field
+// runs the fetch edamame with q=text field value
+// create html elements for data returned 
+// append new html element to location already on html page 
