58c3e9b5eefa801b6d0acbcede2a36b565d8a8b8
areli <77548234+aarelip@users.noreply.github.com>
2021-03-20 12:58:01 -0700

    console log edemame response

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,6 +1,6 @@
 var apiId = `65eb38bf`
 var apiKey = `1c2548d58be7dcb81429580c548dd86d`
-var edamameApi = `https://api.edamam.com/search?q=${$txtField}&app_id=${apiId}&app_key=${apiKey}&from=0&to=3&calories=591-722&health=alcohol-free`
+var edamameApi = `https://api.edamam.com/search?q=pizza&app_id=${apiId}&app_key=${apiKey}&from=0&to=3&calories=591-722&health=alcohol-free`
 
 // fetch(`https://api.edamam.com/search?q=chicken&app_id=${apiId}&app_key=${apiKey}&from=0&to=3&calories=591-722&health=alcohol-free`)
 //     .then(response => response.json())
@@ -13,11 +13,12 @@ console.log($txtField);
 var $btn = document.querySelector("#btn");
 console.log($btn);
 // add an onclick event to button 
-document.getElementById("#btn").addEventListener("click",
+document.getElementById("btn").addEventListener("click",
     // that finds the value of the text field
-    fetch(edamameApi).then(function () {
+    fetch(edamameApi).then(function (response) {
         // runs the fetch edamame with q=text field value
         if (response.ok) {
+            console.log("response",response.body)
             return response.json()
         }
         else {
