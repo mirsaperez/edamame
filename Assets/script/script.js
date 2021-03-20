e85659d610a37be61f0c9f8f2a316dc2583e1ad1
mirsaperez <77262010+mirsaperez@users.noreply.github.com>
2021-03-20 14:29:42 -0700

    fixed search box

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,7 +1,8 @@
 var apiId = `65eb38bf`
 var apiKey = `7ba37096f7d35dd3b5bd8c65c2dfe698`
-var edamameApi = `https://api.edamam.com/search?q=pizza&app_id=${apiId}&app_key=${apiKey}`
+var edamameApi = `https://api.edamam.com/search?app_id=${apiId}&app_key=${apiKey}&q=`
 const $searchform=document.querySelector("#search-form")
+const $resultsContainer=document.querySelector("#results-container")
 
 // fetch(`https://api.edamam.com/search?q=chicken&app_id=${apiId}&app_key=${apiKey}&from=0&to=3&calories=591-722&health=alcohol-free`)
 //     .then(response => response.json())
@@ -19,10 +20,21 @@ $searchform.addEventListener("submit", ( event )=> {
     // that finds the value of the text field
     $.ajax({
         url:
-          edamameApi
+          edamameApi+
+          $txtField.value
       })
         .then(function (response) {
             console.log("response",response)
+            let recipes=response.hits
+            for (let i = 0; i < recipes.length; i++) {
+                const recipe = recipes[i];
+                console.log(recipe)
+                let newTitle=document.createElement("h3")
+                newTitle.innerText=recipe.recipe.label
+                $resultsContainer.appendChild(newTitle)
+                
+              
+            }
         })
     
     
