368da0ae43f58744f7cdf42fa8a36796f0c1484e
angeeg <angeeg@users.noreply.github.com>
2021-03-21 16:55:00 -0700

    added 2nd api and images/url to recipe when recipe is searched

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,8 +1,9 @@
 var apiId = `65eb38bf`
 var apiKey = `7ba37096f7d35dd3b5bd8c65c2dfe698`
 var edamameApi = `https://api.edamam.com/search?app_id=${apiId}&app_key=${apiKey}&q=`
-const $searchform=document.querySelector("#search-form")
-const $resultsContainer=document.querySelector("#results-container")
+var smsApi = `https://www.A-ZSMS.com/api/text`
+const $searchform = document.querySelector("#search-form")
+const $resultsContainer = document.querySelector("#results-container")
 
 // fetch(`https://api.edamam.com/search?q=chicken&app_id=${apiId}&app_key=${apiKey}&from=0&to=3&calories=591-722&health=alcohol-free`)
 //     .then(response => response.json())
@@ -15,41 +16,82 @@ console.log($txtField);
 var $btn = document.querySelector(".btn");
 console.log($btn);
 // add an onclick event to button 
-$searchform.addEventListener("submit", ( event )=> { 
+$searchform.addEventListener("submit", (event) => {
     event.preventDefault()
     // that finds the value of the text field
     $.ajax({
         url:
-          edamameApi+
-          $txtField.value
-      })
+            edamameApi +
+            $txtField.value
+    })
         .then(function (response) {
-            console.log("response",response)
-            let recipes=response.hits
+            console.log("response", response)
+            let recipes = response.hits
             for (let i = 0; i < recipes.length; i++) {
                 const recipe = recipes[i];
-                let newTitle=document.createElement("h3")
-                newTitle.innerText=recipe.recipe.label
+                // append new html element to location already on html page 
+                let newTitle = document.createElement("h3")
+                newTitle.innerText = recipe.recipe.label
                 $resultsContainer.appendChild(newTitle)
                 
-              let ingredients = document.createElement("p")
-              ingredients.textContent = recipe.recipe.ingredientLines
-              $resultsContainer.appendChild(ingredients)
-              console.log("response")
+                let image = document.createElement("img")
+                image.setAttribute("src", recipe.recipe.image) 
+                $resultsContainer.appendChild(image)
+
+                let ingredients = document.createElement("p")
+                ingredients.textContent = recipe.recipe.ingredientLines
+                $resultsContainer.appendChild(ingredients)
+                console.log("response")
+
+                let urlLink = document.createElement("a")
+                urlLink.setAttribute("href", recipe.recipe.url)
+                let url = document.createElement("p")
+                url.textContent = recipe.recipe.url
+                urlLink.appendChild(url)
+                $resultsContainer.appendChild(urlLink)
+                console.log(url)
+                
+                let smsContainer = document.createElement("div")
+    
+                
+                // create SMS button
+                let smsInput = document.createElement("input")
+                smsInput.placeholder = "Enter phone number"
+                smsContainer.appendChild(smsInput)
+
+                let smsButton = document.createElement("button")
+                smsButton.innerText = "Send Recipe"
+                smsButton.addEventListener("click", function(){sendSms(recipe.recipe.url,this)})
+                smsContainer.appendChild(smsButton)
+                $resultsContainer.appendChild(smsContainer)
             }
         })
-    
-    
+
+
 
 })
+function sendSms(link, that) {
+    console.log(that)
+    console.log(that.parentElement)
+    console.log(that.parentElement.childNodes[0])
 
 // create html elements for data returned 
-
-// get reference for sms button 
-
-
+console.log(link)
+var smsData = {
+    number: that.parentElement.childNodes[0].value, 
+    message: "Enjoy this recipe at " + link
+}
 
 // send recipe link to user's phone number entered 
+$.ajax({
+    url:
+        smsApi, 
+        data: smsData,
+        type: 'POST'
+   
+})
+.then(function (response) {
+console.log(response)
+})
+}
 
-
-// append new html element to location already on html page 
